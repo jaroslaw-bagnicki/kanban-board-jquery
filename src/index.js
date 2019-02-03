aa79cb5a544be5eb1f949820a879390978f17a70
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-03 07:17:05 +0100

    Scaffold app into index.html

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-console.log('Test message ...');
 import './styles/main.scss';
+import $ from 'jquery';
+import { Board } from './scripts/models';
+
+const sampleBoardData = {
+  id: Date.now(),
+  $element: $('<div>'),
+  name: 'Test board'
+};
+
+const board = new Board(sampleBoardData);
+board.render();
+
+const appContainer = $('#app-container');
+appContainer.append(board.$element);
