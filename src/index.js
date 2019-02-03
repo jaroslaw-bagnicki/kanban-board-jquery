038713ce6527f9b591aec12f4a8fb4366522c8be
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-03 18:31:49 +0100

    Scaffold sample board for tests

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import './styles/main.scss';
 import $ from 'jquery';
-import { Board } from './scripts/models';
+import { Board, Column, Card } from './scripts/models';
+import { generateId } from './scripts/utils';
 
-const sampleBoardData = {
-  id: Date.now(),
-  $element: $('<div>'),
+const boardData = {
+  id: generateId(),
   name: 'Test board'
 };
 
-const board = new Board(sampleBoardData);
-board.render();
+const colData = { 
+  id: generateId(), 
+  name: 'TO DO' 
+};
 
-const appContainer = $('#app-container');
-appContainer.append(board.$element);
+const cardData = {
+  id: generateId(), 
+  name: 'Task1',
+  description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, exercitationem.',
+  color: 'red'
+};
+
+const board = new Board(boardData);
+const col = new Column(colData);
+const card = new Card(cardData);
+
+function appBootsrtap() {
+  const appContainer = $('#app-container');
+  appContainer.append(board.$element);
+  board.$element.find('.columns-container').append(col.$element);
+  col.$element.find('.cards-container').append(card.$element);
+}
+
+$(function() {
+  appBootsrtap();
+});
